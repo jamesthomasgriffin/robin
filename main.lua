9fdb07eab3e793ff4c30896e8c733ee370349056
James Griffin <james.griffin@cantab.net>
2026-07-29 17:32:18 -0400

    Support multiple modes for comparisons

--- main.lua
+++ main.lua
@@ -1,46 +1,13 @@
 local robin = require 'robin'
 local utf8 = require 'utf8'
 
-local sampleChinese = [[ 人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは、世界における自由、正義及び平和の基礎であるので、
+local sampleChinese = [[人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは、世界における自由、正義及び平和の基礎であるので、
 人権の無視及び軽侮が、人類の良心を踏みにじった野蛮行為をもたらし、言論及び信仰の自由が受けられ、恐怖及び欠乏のない世界の到来が、一般の人々の最高の願望として宣言されたので、
 人間が専制と圧迫とに対する最後の手段として反逆に訴えることがないようにするためには、法の支配によって人権を保護することが肝要であるので、
 諸国間の友好関係の発展を促進することが肝要であるので、国際連合の諸国民は、国連憲章において、基本的人権、人間の尊厳及び価値並びに男女の同権についての信念を再確認し、かつ、一層大きな自由のうちで社会的進歩と生活水準の向上とを促進することを決意したので、]]
 local sampleEnglish = [[Everyone has the right to freedom of thought, conscience and religion; this right includes freedom to change his religion or belief, and freedom, either alone or in community with others and in public or private, to manifest his religion or belief in teaching, practice, worship and observance.
 Everyone has the right to freedom of opinion and expression; this right includes freedom to hold opinions without interference and to seek, receive and impart information and ideas through any media and regardless of frontiers.
 Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays with pay.]]
-
-local function isWhitespace(cp)
-  local lookup = {
-      -- ASCII whitespace
-      [9]   = true, -- Horizontal Tab (\t)
-      [10]  = true, -- Line Feed (\n)
-      [11]  = true, -- Vertical Tab (\v)
-      [12]  = true, -- Form Feed (\f)
-      [13]  = true, -- Carriage Return (\r)
-      [32]  = true, -- Space
-
-      -- Unicode whitespace
-      [0x00A0] = true, -- No-break space
-      [0x1680] = true, -- Ogham space mark
-      [0x2000] = true, -- En quad
-      [0x2001] = true, -- Em quad
-      [0x2002] = true, -- En space
-      [0x2003] = true, -- Em space
-      [0x2004] = true, -- Three-per-em space
-      [0x2005] = true, -- Four-per-em space
-      [0x2006] = true, -- Six-per-em space
-      [0x2007] = true, -- Figure space
-      [0x2008] = true, -- Punctuation space
-      [0x2009] = true, -- Thin space
-      [0x200A] = true, -- Hair space
-      [0x2028] = true, -- Line separator
-      [0x2029] = true, -- Paragraph separator
-      [0x202F] = true, -- Narrow no-break space
-      [0x205F] = true, -- Medium mathematical space
-      [0x3000] = true, -- Ideographic (CJK) space
-  }
-  return lookup[cp] or false
-end
 
 -- A rough proof of concept
 local RobinText = {}
@@ -106,10 +73,11 @@ function RobinText:draw(pass, text, wrap)
   pass:scale(1.0 / self.font:getPixelDensity())
   local instanceOffset = self.instanceBuffer.size    
   
-  local leading = -self.rasterizer:getLeading()
-  local count = 0
+  local lineHeight = -self.rasterizer:getLeading() * self.font:getLineSpacing()
+  local lineOffset = -self.rasterizer:getAscent()
+  local instanceCount = 0
   
-  local lines = self.font:getLines(text, wrap or 1e6)
+  local lines = self.font:getLines(text, wrap or 1e16)
   for lineIndex, line in ipairs(lines) do
     local lastCodepoint = nil
     local advance = 0
@@ -123,9 +91,9 @@ function RobinText:draw(pass, text, wrap)
           advance = advance + self.rasterizer:getKerning(lastCodepoint, codepoint)
         end
         
-        if not (isWhitespace(codepoint) or entry.skip) then
-          count = count + 1
-          self.instanceBuffer:pushback(advance, (lineIndex - 1) * leading, entry.index)  
+        if not entry.skip then
+          instanceCount = instanceCount + 1
+          self.instanceBuffer:pushback(advance, (lineIndex-1) * lineHeight + lineOffset, entry.index)
         end
         
         advance = advance + entry.advance      
@@ -135,32 +103,62 @@ function RobinText:draw(pass, text, wrap)
   end
   
   pass:setShader(robin.shader)
-  pass:setBlendMode('alpha')
-  pass:setDepthTest('none')
   self.robinBuffer:sendBuffers(pass)
   pass:send("instanceOffset", instanceOffset)
   pass:send("GlyphInstances", self.instanceBuffer.buffer)
-  pass:draw(self.mesh, nil, count)
+  pass:draw(self.mesh, nil, instanceCount)
   pass:pop()
 end
-  
 
-function lovr.draw(pass) 
-  robinText.instanceBuffer:clear()
+local modes = {none = "None", both = "Both MSDF and ROBIN", msdf = "MSDF (64x64)", robin = "ROBIN" }
+modes.keys = { u = modes.none, r = modes.robin, t = modes.msdf, y = modes.both }
+modes.selected = modes.robin
   
-  lovr.graphics.setBackgroundColor(0.9, 0.9, 0.9)
-  pass:push()
-  pass:setColor(0, 0, 0)
-  
-  local blockWidth = 16
-  pass:translate(-blockWidth / 2, 1.7, -10)
-  
-  robinText:draw(pass, robinText.sample, blockWidth)
-  pass:pop()
+function lovr.draw(pass)
+  lovr.graphics.setBackgroundColor(1, 1, 1)
+  local mode = modes.selected
+    
+  if not (mode == modes.none) then
+    robinText.instanceBuffer:clear()
+    
+    local text = robinText.sample
+    pass:setFont(robinText.font)
+    
+    pass:push()
+    pass:setBlendMode('alpha')
+    pass:setDepthTest('none')
+    
+    local blockWidth = 12
+    pass:translate(-blockWidth / 2, 1.7, -10)
+    
+    
+    local robin, msdf, both = mode == modes.robin, mode == modes.msdf, mode == modes.both
+    
+    robinText.font:setLineSpacing(1.0)
+    if msdf or both then
+      if both then
+        pass:setColor(0.5, 0.1, 0.1)
+        robinText.font:setLineSpacing(2.0)
+      else      
+        pass:setColor(0, 0, 0)
+      end
+      pass:text(text, 0, 0, 0, 1.0, 0, 0, 0, 1, blockWidth, "left", "top")
+    end
+    
+    if robin or both then
+      if both then -- interleave
+        pass:translate(0, robinText.font:getHeight(), 0)
+      end
+      pass:setColor(0, 0, 0)
+      robinText:draw(pass, text, blockWidth)
+    end
+    pass:pop()
+  end
   
   local stats = pass:getStats()
   local text = {
-    "Press number keys to select font from fonts folder",
+    "Mode: "..mode.." (R, T, Y, U) to change, F for fullscreen",
+    string.format("Press 1 to %d to select font from folder (or 0 is default) ", #loadedFonts),
     "Use (shift) +/- to change entry dimensions",
     string.format("%.2fms", stats.gpuTime * 1000),
     string.format("Draws: %d", stats.draws),
@@ -172,27 +170,26 @@ end
 
 function displayInfo(pass, text)
 
-  local textSize = 0.8
-  local lineSpacing = 0.04
+  local textSize = 0.4
   pass:push()
+  
+  pass:setBlendMode('alpha')
+  pass:setDepthTest('none')
   pass:setShader()
   pass:setViewPose(1, mat4():identity())
   pass:setProjection('orthographic')
-  pass:setDepthTest('none')
-  pass:setBlendMode()
+  pass:scale(1, -1)
   
   local width, height = pass:getDimensions()
   
-  pass:scale(textSize, -textSize, 1)
-  pass:translate(1.5 * lineSpacing * width, -1.5 * lineSpacing * height, 0)
+  pass:scale(textSize)
+  robinText.font:setLineSpacing(0.8)
+  pass:translate(0.05 * width, -0.04 * height, 0)
   pass:setColor(0x332211)
   local savedPD = robinText.font:getPixelDensity()
   robinText.font:setPixelDensity(1)
-  --pass:scale(32, 32)
-  for no, line in ipairs(text) do
-    robinText:draw(pass, line)
-    pass:translate(0, -1 * lineSpacing * height, 0)
-  end
+  
+  robinText:draw(pass, table.concat(text, '\n'))
   pass:pop()
   robinText.font:setPixelDensity(savedPD)
 end
@@ -215,7 +212,7 @@ function lovr.load()
   for _, filename in ipairs(lovr.filesystem.getDirectoryItems("fonts/")) do
     if string.match(filename, "%.[tT][tT][fF]$") then
       local path = "fonts/" .. filename
-      local font = lovr.graphics.newFont(path)
+      local font = lovr.graphics.newFont(path, 64)
       if font then
         table.insert(loadedFonts, font)
         print("Loaded ", path)
@@ -229,32 +226,43 @@ function lovr.load()
 end
 
 function lovr.keypressed(key, scancode, isrepeat)
+  print("Keypress", key)
+  
   local entryWidth, entryHeight = robinText.robinBuffer.entryWidth, robinText.robinBuffer.entryHeight
   
   local originalFontIndex = robinText.fontIndex
   local originalW, originalH = entryWidth, entryHeight
   
-  local shift = lovr.system.isKeyDown("lshift", "rshift")
+  local shiftDown = lovr.system.isKeyDown("lshift", "rshift")
   
   if key == "=" then
-    if shift then
+    if shiftDown then
       entryHeight = math.min(entryHeight * 2, 64)
     else
       entryWidth = math.min(entryWidth * 2, 64)
     end
   elseif key == "-" then
-    if shift then
+    if shiftDown then
       entryHeight = math.max(entryHeight / 2, 1)
     else
       entryWidth = math.max(entryWidth / 2, 1)
     end
   end
+  
+  local newMode = modes.keys[key]
+  modes.selected = newMode or modes.selected  
 
   local fontIndex = tonumber(key) or originalFontIndex
   
   if originalW ~= entryWidth or originalH ~= entryHeight 
     or originalFontIndex ~= fontIndex then
       setActiveFont(fontIndex, entryWidth, entryHeight)
+  end
+  
+  if key == 'f' then
+    lovr.system.setWindowFullscreen(not lovr.system.isWindowFullscreen())
+  elseif key == 'escape' then
+    lovr.event.quit()
   end
 end
 

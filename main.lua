c263a6ddb3058dc1f2f425aa392d01e694c3844e
James Griffin <james.griffin@cantab.net>
2026-08-05 15:49:06 -0400

    Small refinements

--- main.lua
+++ main.lua
@@ -1,11 +1,7 @@
 local robin = require 'robin'
 local utf8 = require 'utf8'
 
-local sampleChinese = [[人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは、世界における自由、正義及び平和の基礎であるので、
-人権の無視及び軽侮が、人類の良心を踏みにじった野蛮行為をもたらし、言論及び信仰の自由が受けられ、恐怖及び欠乏のない世界の到来が、一般の人々の最高の願望として宣言されたので、
-人間が専制と圧迫とに対する最後の手段として反逆に訴えることがないようにするためには、法の支配によって人権を保護することが肝要であるので、
-諸国間の友好関係の発展を促進することが肝要であるので、国際連合の諸国民は、国連憲章において、基本的人権、人間の尊厳及び価値並びに男女の同権についての信念を再確認し、かつ、一層大きな自由のうちで社会的進歩と生活水準の向上とを促進することを決意したので、]]
-local sampleEnglish = [[Everyone has the right to freedom of thought, conscience and religion; this right includes freedom to change his religion or belief, and freedom, either alone or in community with others and in public or private, to manifest his religion or belief in teaching, practice, worship and observance.
+local sample = [[Everyone has the right to freedom of thought, conscience and religion; this right includes freedom to change his religion or belief, and freedom, either alone or in community with others and in public or private, to manifest his religion or belief in teaching, practice, worship and observance.
 Everyone has the right to freedom of opinion and expression; this right includes freedom to hold opinions without interference and to seek, receive and impart information and ideas through any media and regardless of frontiers.
 Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays with pay.]]
 
@@ -33,8 +29,6 @@ function RobinText.new(font, entryWidth, entryHeight)
     rasterWidth = rasterW, rasterHeight = rasterH})
   o.characters = { count = 0 }
   
-  o.sample = o.rasterizer:hasGlyphs(0x4E00) and sampleChinese or sampleEnglish
-
   o.instanceBuffer = robin.InstanceBuffer.new()
   o.mesh = lovr.graphics.newMesh({
     { 'VertexPosition', 'vec3' },
@@ -113,6 +107,7 @@ end
 local modes = {none = "None", both = "Both MSDF and ROBIN", msdf = "MSDF (64x64)", robin = "ROBIN" }
 modes.keys = { u = modes.none, r = modes.robin, t = modes.msdf, y = modes.both }
 modes.selected = modes.robin
+modes.showStats = true
   
 function lovr.draw(pass)
   lovr.graphics.setBackgroundColor(1, 1, 1)
@@ -121,7 +116,6 @@ function lovr.draw(pass)
   if not (mode == modes.none) then
     robinText.instanceBuffer:clear()
     
-    local text = robinText.sample
     pass:setFont(robinText.font)
     
     pass:push()
@@ -142,7 +136,7 @@ function lovr.draw(pass)
       else      
         pass:setColor(0, 0, 0)
       end
-      pass:text(text, 0, 0, 0, 1.0, 0, 0, 0, 1, blockWidth, "left", "top")
+      pass:text(sample, 0, 0, 0, 1.0, 0, 0, 0, 1, blockWidth, "left", "top")
     end
     
     if robin or both then
@@ -150,27 +144,30 @@ function lovr.draw(pass)
         pass:translate(0, robinText.font:getHeight(), 0)
       end
       pass:setColor(0, 0, 0)
-      robinText:draw(pass, text, blockWidth)
+      robinText:draw(pass, sample, blockWidth)
     end
     pass:pop()
   end
   
-  local stats = pass:getStats()
-  local text = {
-    "Mode: "..mode.." (R, T, Y, U) to change, F for fullscreen",
-    string.format("Press 1 to %d to select font from folder (or 0 is default) ", #loadedFonts),
-    "Use (shift) +/- to change entry dimensions",
-    string.format("%.2fms", stats.gpuTime * 1000),
-    string.format("Draws: %d", stats.draws),
-    string.format("Entry size: %d x %d", robinText.robinBuffer.entryWidth, robinText.robinBuffer.entryHeight),
-    string.format("Memory Factor: %.2f", robinText.robinBuffer:getMemoryEfficiency())
-  }
-  displayInfo(pass, table.concat(text, '\n'))
+  if modes.showStats then
+    local stats = pass:getStats()
+    local text = {
+      "Mode: "..mode.." (R, T, Y, U) to change, F for fullscreen",
+      string.format("Press 1 to %d to select font from folder (or 0 is default) ", #loadedFonts),
+      "Use (shift) +/- to change entry dimensions",
+      string.format("%.2fms", stats.gpuTime * 1000),
+      string.format("Draws: %d", stats.draws),
+      string.format("Entry size: %d x %d", robinText.robinBuffer.entryWidth, robinText.robinBuffer.entryHeight),
+      string.format("Memory Factor: %.2f", robinText.robinBuffer:getMemoryEfficiency())
+    }
+    displayInfo(pass, table.concat(text, '\n'))
+  end
 end
 
 function displayInfo(pass, text)
 
-  local textSize = 0.4
+  local font = lovr.graphics.getDefaultFont()
+  local textSize = 0.8
   pass:push()
   
   pass:setBlendMode('alpha')
@@ -179,17 +176,19 @@ function displayInfo(pass, text)
   pass:setViewPose(1, mat4():identity())
   pass:setProjection('orthographic')
   pass:scale(1, -1)
+  pass:setFont(font)
+  
   
   local width, height = pass:getDimensions()
   
   pass:scale(textSize)
-  robinText.font:setLineSpacing(0.8)
+  font:setLineSpacing(0.8)
   pass:translate(0.05 * width, -0.04 * height, 0)
   pass:setColor(0x332211)
-  local savedPD = robinText.font:getPixelDensity()
-  robinText.font:setPixelDensity(1)
+  local savedPD = font:getPixelDensity()
+  font:setPixelDensity(1)
   
-  if modes.selected == modes.msdf then
+  if true or modes.selected == modes.msdf then
     pass:scale(1, -1)
     pass:text(text, 0, 0, 0, 1, 0, 0, 0, 1, 1e16, "left", "top")
   else
@@ -197,7 +196,7 @@ function displayInfo(pass, text)
   end
   
   pass:pop()
-  robinText.font:setPixelDensity(savedPD)
+  font:setPixelDensity(savedPD)
 end
 
 local function setActiveFont(k, entryWidth, entryHeight)
@@ -267,6 +266,8 @@ function lovr.keypressed(key, scancode, isrepeat)
   
   if key == 'f' then
     lovr.system.setWindowFullscreen(not lovr.system.isWindowFullscreen())
+  elseif key == 'h' then
+    modes.showStats = not modes.showStats
   elseif key == 'escape' then
     lovr.event.quit()
   end
